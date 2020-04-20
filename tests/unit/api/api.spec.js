49ec832e0667a60396315e4262b20ddde8c863e1
vivanov <bob465447@gmail.com>
2020-04-20 20:48:33 +0400

    ngf

--- tests/unit/api/api.spec.js
+++
@@ -1,9 +0,0 @@
-import { shallowMount } from "@vue/test-utils";
-import { Users } from "../../../src/api/";
-
-describe("API: Users", () => {
-  it("getUserData", async () => {
-    const RESPONSE = await Users.getUserData();
-    expect(RESPONSE).toMatchSnapshot();
-  });
-});
